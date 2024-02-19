c2ef1aec7a9f822065bd6ec12e2d04b58f699b9b
Anar Agaev <anar.n.agaev@gmail.com>
2024-02-19 18:39:38 +0300

    Added remove selected for Fan modal filters

--- main.js
+++ main.js
@@ -56,10 +56,8 @@ const hideAllFilterTooltips = () => {
 
 const getPositionFiltersTooltipsActions = (node) => {
     var rect = node.getBoundingClientRect()
-    var scrollLeft = window.pageXOffset || document.documentElement.scrollLeft
-    var scrollTop = window.pageYOffset || document.documentElement.scrollTop
-    var left = rect.left + scrollLeft
-    var top = rect.top + scrollTop
+    var left = rect.left
+    var top = rect.top
     return { left: left, top: top }
 }
 
@@ -199,6 +197,131 @@ const initFiltersFilters = () => {
     ))
 }
 
+// Periods
+const setFiltersInputFromRange = (node) => {
+    const val = node.value
+    const name = node.name
+    const type = node.dataset.target
+    const target = document.querySelector(`[data-range-name="${name}"][data-range-type="${type}"]`)
+    target.value = val
+}
+
+const setFiltersRangeView = (node, val) => {
+    if (!val) val = node.value
+
+    const type = node.dataset.target
+    const bar = node.parentNode.querySelector('.bar')
+    const slide = node.parentNode.querySelector('.slide')
+    const max = node.max - node.min
+    const value = val - node.min
+    const percentValue = value / max * 100
+
+    slide.style.left = `${percentValue}%`
+
+    bar.style.width = type === 'min'
+        ? `calc(${100 - percentValue}% + 20px)`
+        : `calc(${percentValue}% + 20px)`
+}
+
+const fixFiltesInputRangeValue = (val) => {
+    if (val === '.') val = ''
+
+    if (val === '0') val = ''
+
+    val = val.replace(/[^0-9.]/g, "") // вырезаем все кроме цифр и точки
+    val = val.replace(/\.+/g, '.') // вырезаем несколько точек подряд
+
+    if ((val.match(/\./g) || []).length > 1) { // если количество точке (не подряд) блее одной
+        val = val.replace(/\.$/, '') // отрезаем точку в конце строки
+    }
+
+    return val
+}
+
+const handleBlurPeriodInput = (node, e) => {
+    const type = node.dataset.rangeType
+    const name = node.dataset.rangeName
+    const target = document.querySelector(`[name="${name}"][data-target="${type}"]`)
+    let val = node.value
+
+    if (val === '') val = type === 'min'
+        ? target.min
+        : target.max
+
+    if (parseFloat(val) < parseFloat(target.min)) val = target.min
+    if (parseFloat(val) > parseFloat(target.max)) val = target.max
+
+    node.value = val
+    setFiltersRangeView(target, val)
+}
+
+
+const initFiltersPeriods = () => {
+    const ranges = Array.from(document.querySelectorAll('[type="range"]'))
+    ranges.forEach(range => range.addEventListener('input', function() {
+        setFiltersInputFromRange(this)
+        setFiltersRangeView(this)
+    }))
+
+    const inputs = Array.from(document.querySelectorAll('.filters__priod-values [type="text"]'))
+    inputs.forEach(input => {
+        input.addEventListener('input', function(e) {
+            this.value = fixFiltesInputRangeValue(this.value)
+        })
+
+        input.addEventListener('keypress', function(e) {
+            if (e.key === "Enter") this.blur()
+        })
+
+        input.addEventListener('blur', function(e) {
+            handleBlurPeriodInput(this, e)
+        })
+    })
+}
+
+// Seletcs
+const checkFiltersResetBadges = () => {
+    const nodes = Array.from(document
+        .querySelectorAll('.filters__selected button:not(.filters__reset)'))
+
+    const selected = document.querySelector('.filters__selected')
+
+    if (nodes.length === 0) selected.classList.add('invisible')
+}
+
+const initFiltersRemoveSelected = () => {
+    const nodes = Array.from(document
+        .querySelectorAll('.filters__selected button:not(.filters__reset)'))
+
+    nodes.forEach(node => node.addEventListener('click', function() {
+        this.parentNode.remove()
+        checkFiltersResetBadges()
+    }))
+}
+
+// Toggle filter modal
+const initFiltersModal = () => {
+    const close = document.querySelector('.filters__close')
+    const toggler = document.querySelector('.filters-toggler')
+    const modal = document.getElementById('filters')
+
+    const toggleFilter = () => {
+        if (modal.classList.contains('show')) {
+            document.body.classList.remove('filters-opem')
+            modal.classList.remove('visible')
+            setTimeout(() => modal.classList.remove('show'), 100)
+            return
+        }
+
+        document.body.classList.add('filters-opem')
+        modal.classList.add('show')
+        setTimeout(() => modal.classList.add('visible'), 100)
+    }
+
+    close.addEventListener('click', toggleFilter)
+    toggler.addEventListener('click', toggleFilter)
+}
+
 window.addEventListener('load', () => {
     const isFilters = document.querySelector('.filters')
     if (!isFilters) return
@@ -216,4 +339,13 @@ window.addEventListener('load', () => {
 
     // Searchig
     initFiltersFilters()
+
+    // Periods
+    initFiltersPeriods()
+
+    // Seletcs
+    initFiltersRemoveSelected()
+
+    // Toggle filter modal
+    initFiltersModal()
 })
